d284dee4060fa9f67ea22a7767fbe2c9425bef76
PaintNinja <paintninjamail-projects@yahoo.co.uk>
2016-03-30 20:55:23 +0100

    Fix indentation and add graceful script quitting

--- PyWaveThinclient-DEV.py
+++ PyWaveThinclient-DEV.py
@@ -74,30 +74,30 @@ staticTime = str(formattedTime)
 # Note: Apps by default are stored in /usr/bin on Linux and %ProgramFiles% on Windows
 if os.path.isdir(str(BINDIR) + "\Wave\cache") == False:
     # if the directory doesn't exist, create it
-	os.makedirs(str(BINDIR) + "\Wave\cache", exist_ok=True)
-	# Note: exist_ok tells Python that it's okay if the directory already exists when we attempt to create it, so don't throw an error and crash because of that. However, this feature requires Python v3.4.1+
-	
+    os.makedirs(str(BINDIR) + "\Wave\cache", exist_ok=True)
+    # Note: exist_ok tells Python that it's okay if the directory already exists when we attempt to create it, so don't throw an error and crash because of that. However, this feature requires Python v3.4.1+
+    
 import platform
 if str(platform.system()) == "Windows":
     # Hide the cache directory from non-pro-users once made on Windows to help prevent mistakes and mess caused by users deleting the folders.
-	os.system('attrib +H /S /D ' + str(BINDIR) + "\Wave\cache")
-	
+    os.system('attrib +H /S /D ' + str(BINDIR) + "\Wave\cache")
+    
 # Check if WebappCaching is enabled
 if WebappCaching == True:
     verifyTmp = "\\Wave\\cache\\" + str(arg0)
-	# Note: Double backslashes are needed as the backslash character is the escape character in Python
+    # Note: Double backslashes are needed as the backslash character is the escape character in Python
 else:
     import random
     verifyTmp = "\\Wave\\cache\\" + staticTime + "-" + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
-	# Make verifyTmp contain the captured staticTime from earlier, then a dash and 4 random numbers. The numbers have to be converted to a string with str() in order to stop Python from crashing as usual
+    # Make verifyTmp contain the captured staticTime from earlier, then a dash and 4 random numbers. The numbers have to be converted to a string with str() in order to stop Python from crashing as usual
 
 if os.path.isdir(str(BINDIR) + str(verifyTmp)) == False:
     # make the verifyTmp directory
     os.makedirs(str(BINDIR) + str(verifyTmp), exist_ok=True)
-	
+    
 if str(platform.system()) == "Windows":
     # Hide the verifyTmp directory if on Windows
-	os.system('attrib +H /S /D ' + str(BINDIR) + str(verifyTmp))
+    os.system('attrib +H /S /D ' + str(BINDIR) + str(verifyTmp))
 
 
 
@@ -111,12 +111,12 @@ print("Authenticating...")
 # Check if the webapp exists already. If not, get it with cURL.
 if os.path.exists(str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp") == False:
     # Get it on Windows using cURL.exe
-	if str(platform.system()) == "Windows":
-	    os.system("call " + str(BINDIR) + '\\Wave\\libs\\cURL.exe -H -http2 --tcp-nodelay -A "PyWave/v0.10 (Pre-Alpha)" -s -o ' + str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp " + VerifyServ + "/" + str(arg0) + ".webapp")
-	
-	# Get it on Linux using cURL (needs testing)
-	if str(platform.system()) == "Linux":
-	    os.system('curl -H -http2 --tcp-nodelay -A "PyWave/v0.10 (Pre-Alpha)" -s -o ' + str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp " + VerifyServ + "/" + str(arg0) + ".webapp")
+    if str(platform.system()) == "Windows":
+        os.system("call " + str(BINDIR) + '\\Wave\\libs\\cURL.exe -H -http2 --tcp-nodelay -A "PyWave/v0.10 (Pre-Alpha)" -s -o ' + str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp " + VerifyServ + "/" + str(arg0) + ".webapp")
+    
+    # Get it on Linux using cURL (needs testing)
+    if str(platform.system()) == "Linux":
+        os.system('curl -H -http2 --tcp-nodelay -A "PyWave/v0.10 (Pre-Alpha)" -s -o ' + str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp " + VerifyServ + "/" + str(arg0) + ".webapp")
 
 if os.path.exists(str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp") == False:
     # todo: change colour to red text on black background
@@ -127,12 +127,11 @@ if os.path.exists(str(BINDIR) + str(verifyTmp) + "\\" + str(arg0) + ".webapp") =
     print("              \Download webapp")
     import shutil
     shutil.rmtree(str(BINDIR) + str(verifyTmp), ignore_errors=True)
-	# delete the verifyTmp directory
-	
-	if str(platform.system()) == "Windows":
-	    os.system("pause")
-	else:
-	    input("Press any key and hit enter to continue . . .")
-	# todo: pause console here
-	# todo: change colour back to white text on black background
-	# todo: quit gracefully
+    # delete the verifyTmp directory
+    
+    if str(platform.system()) == "Windows":
+        os.system("pause")
+    else:
+        input("Press any key and hit enter to continue . . .")
+    # todo: change colour back to white text on black background
+    sys.exit()
